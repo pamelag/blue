113fdbfd161e2b8bc2e690cc8bad2d727f6cf192
pamelag <pamela.gangopadhyay@gmail.com>
2019-05-13 10:42:20 +1000

    feat: changed json response from article_id to id

--- server/article.go
+++ server/article.go
@@ -83,7 +83,7 @@ func (h *articleHandler) getArticle(w http.ResponseWriter, r *http.Request) {
 	} */
 
 	var response = struct {
-		ID    int      `json:"article_id"`
+		ID    int      `json:"id"`
 		Title string   `json:"title"`
 		Date  string   `json:"date"`
 		Body  string   `json:"body"`
